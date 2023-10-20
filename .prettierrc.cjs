086cac393c765a8e402bdad18994e245df5764a2
Tanya Rashid <trashid@jigsawcreate.com>
2023-10-20 15:35:52 +0100

    prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  endOfLine: 'auto',
+  quoteProps: 'consistent',
+  printWidth: 80,
+  vueIndentScriptAndStyle: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
